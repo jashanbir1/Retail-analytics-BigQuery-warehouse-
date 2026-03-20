0413ae52e8b37e2588c04eafaa0c5f5ce645063a
jashanbir <jashanbir24@hotmail.com>
2026-03-19 23:52:45 -0700

    made update to track avg_rev_per_order and avg_units_per_order in gold_product sales

--- shopify_retail_dbt/models/example/gold/marts/gold_product_sales.sql
+++ shopify_retail_dbt/models/example/gold/marts/gold_product_sales.sql
@@ -9,7 +9,9 @@ SELECT
     dim.vendor,
     count(distinct fct.order_id) as total_orders,
     sum(fct.quantity) as total_units_sold,
-    sum(fct.line_revenue) as all_time_revenue
+    sum(fct.line_revenue) as all_time_revenue,
+    SAFE_DIVIDE(SUM(fct.line_revenue), COUNT(DISTINCT fct.order_id)) AS avg_revenue_per_order,
+    SAFE_DIVIDE(SUM(fct.quantity), COUNT(DISTINCT fct.order_id)) AS avg_units_per_order
 
 FROM {{ ref('fact_order_line_items')}} as fct
 INNER JOIN {{ ref('dim_products')}} as dim
